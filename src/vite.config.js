2782f5e297d3ca756391da88697c5ae3b971ac7b
Antoine Eskaros <tonyraoul6699@gmail.com>
2026-05-29 15:20:33 +1000

    .

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({
+
+export default defineConfig(({ command }) => ({
+    base: command === "build" ? "/station-alpha/" : "/",
     plugins: [react()],
-});
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+    },
+}));
